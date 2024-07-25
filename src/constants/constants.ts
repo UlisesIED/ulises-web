11b93f6614da9a3522edd1e0227afd018bd35f7b
UlisesIED <uliescodi@gmail.com>
2024-07-25 14:26:32 -0600

    correccion logoss

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,21 +1,34 @@
+import boot from '../../public/assets/images/boot.svg'
+import css from '../../public/assets/images/css.svg'
+import django from '../../public/assets/images/django.svg'
+import git from '../../public/assets/images/git.svg'
+import html from '../../public/assets/images/html.svg'
+import javascript from '../../public/assets/images/javascript.svg'
+import micro from '../../public/assets/images/microservices.svg'
+import next from '../../public/assets/images/next.svg'
+import postgres from '../../public/assets/images/postgres.svg'
+import python from '../../public/assets/images/python.svg'
+import react from '../../public/assets/images/react.svg'
+import ts from '../../public/assets/images/ts.svg'
+
 
 export const specializations = [
     [
-        '/assets/images/python.svg',
-        '/assets/images/postgres.svg',
-        '/assets/images/javascript.svg',
-        '/assets/images/html.svg'
+        python,
+        postgres,
+        javascript,
+        html
     ],
     [
-        '/assets/images/django.svg',
-        '/assets/images/react.svg',
-        '/assets/images/ts.svg',
-        '/assets/images/css.svg'
+        django,
+        react,
+        ts,
+        css
     ],
     [
-        '/assets/images/microservices.svg',
-        '/assets/images/next.svg',
-        '/assets/images/git.svg',
-        '/assets/images/boot.svg'
+        micro,
+        next,
+        git,
+        boot
     ]
 ]
